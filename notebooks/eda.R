4b38a741449972407239fd445b438211b14e48dd
Gianluca Morena <morenagianluca27@gmail.com>
2026-09-18 21:29:24 +0200

    feat: create eda script

---
+++ notebooks/eda.R
@@ -0,0 +1,10 @@
+#libraries for visualization
+library(ggplot2)
+library(dplyr)
+library(janitor)
+library(scales)
+library(ggcorrplot)
+#import dataset from the script "import_dataset.R" in the folder R
+source("R/import_dataset.R")
+
+# visualizasion of categorica
